344f52968ab0f239cc13abb71e0a9b1ee6b38a21
edwin <musyokaedwin01@gmail.com>
2025-11-26 22:49:53 +0300

    Nested Function

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
+
+  return (paaswordAttempt) => {
+
+    if (attemptCount >= 3) {
+      return "Account locked due to too many failed login attempts";
+      
+    }
+    if(paaswordAttempt === userInfo.password) {
+      return"Login successful";
+    }
+
+
+    attemptCount++;
+
+    if (attemptCount <=3 ) {
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+
+
+    return "Account locked"
+
+  };
+}
+
+
 
 
 
